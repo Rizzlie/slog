7fc38801ad60442707fc5e2e2b6d819c64a9e747
Maciej Syguła <mac.sygula@gmail.com>
2023-09-07 15:57:05 +0200

    Add styling packages, create footer, header and basic layout

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+	import * as config from '$lib/config';
+</script>
+
+<nav>
+	<a href="/" class="title">
+		<b>
+			{config.title}
+		</b>
+	</a>
+
+	<ul>
+		<li>
+			<a href="/about"> About </a>
+		</li>
+		<li>
+			<a href="/contact"> Contact </a>
+		</li>
+	</ul>
+
+	<button>Toggle</button>
+</nav>
+
+<style>
+	nav {
+		padding-block: var(--size-7);
+	}
+
+	ul {
+		margin-block: var(--size-7);
+	}
+
+	a {
+		color: inherit;
+		text-decoration: none;
+	}
+</style>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import 'open-props/style';
+	import 'open-props/normalize';
+	import 'open-props/buttons';
+
+	import Header from '$components/Header.svelte';
+	import Footer from '$components/Footer.svelte';
+</script>
+
+<Header />
+
+<slot />
+
+<Footer />
